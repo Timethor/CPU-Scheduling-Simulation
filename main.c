36882dbccfdcffa05a29274a1b1a4365ffd7f0fe
Timethor <Nick.o.rich@gmail.com>
2013-09-20 20:49:40 -0400

    Initial Commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Timethor
+ *
+ * Created on September 20, 2013, 8:45 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
