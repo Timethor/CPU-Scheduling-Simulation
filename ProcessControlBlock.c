fa4fca81dc6067c5b6eb3ea734552e819456de63
Timethor <Nick.o.rich@gmail.com>
2013-09-21 22:14:24 -0400

    pcb and vcpu start

--- ProcessControlBlock.c
+++ ProcessControlBlock.c
@@ -1,1 +1,13 @@
+#include "stdlib.h"
+#include "Dequeue.h"
+#include "ProcessControlBlock.h"
 
+//#define DEBUG
+#include "debug.h"
+
+DEQUEUE(PCB);
+
+// Comparing functions for qsort to use
+int compare_Job_Arrival(const PCB** job1, const PCB** job2){
+	return ((*job1)->arrival_time)-((*job2)->arrival_time);
+}
