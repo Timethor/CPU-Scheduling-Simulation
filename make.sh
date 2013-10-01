e420b2d5674b108b1e62708bd6fc4f826791f062
Timethor <Nick.o.rich@gmail.com>
2013-10-01 08:17:20 -0400

    Ease of use

---
+++ make.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# 
+# File:   make.sh
+# Author: Timethor
+#
+# Created on Oct 1, 2013, 8:13:18 AM
+#
+/usr/bin/gmake -f nbproject/Makefile-Debug_ProcA.list.mk QMAKE= SUBPROJECTS= .build-conf
---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# 
+# File:   run.sh
+# Author: Timethor
+#
+# Created on Oct 1, 2013, 8:13:59 AM
+#
+./dist/Debug_ProcA.list/GNU-Linux-x86/cpu_scheduler_simulator $1
