6f3122d08294c6af45f3bafcfb70a6d258159b52
Timethor <Nick.o.rich@gmail.com>
2013-10-06 18:39:13 -0400

    ease of use

---
+++ make.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# 
+# File:   make.sh
+# Author: Timethor
+#
+# Created on Oct 1, 2013, 8:13:18 AM
+#
+/usr/bin/gmake -f nbproject/Makefile-Debug.mk QMAKE= SUBPROJECTS= .build-conf
---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# 
+# File:   run.sh
+# Author: Timethor
+#
+# Created on Oct 1, 2013, 8:13:59 AM
+#
+./dist/Debug/GNU-Linux-x86/cpu_scheduler_simulator $1 $2 $3 $4
