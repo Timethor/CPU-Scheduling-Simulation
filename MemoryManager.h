301260220f5b0c31185a95a25ff720d81a8dd426
Timethor <Nick.o.rich@gmail.com>
2013-11-07 16:24:24 -0500

    P2: Added Memory Manager File, not implemented

---
+++ MemoryManager.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   MemoryManager.h
+ * Author: Timethor
+ *
+ * Created on November 7, 2013, 4:23 PM
+ */
+
+#ifndef MEMORYMANAGER_H
+#define	MEMORYMANAGER_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MEMORYMANAGER_H */
+
