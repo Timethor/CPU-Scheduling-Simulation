0ac44d81199aaadc3857837c7c12cb8cb6f1d29c
Timethor <Nick.o.rich@gmail.com>
2013-11-07 15:20:15 -0500

    P2: Added MemoryRegion files, not implemented

---
+++ MemoryRegion.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   MemoryRegion.h
+ * Author: Timethor
+ *
+ * Created on November 7, 2013, 6:20 AM
+ */
+
+#ifndef MEMORYREGION_H
+#define	MEMORYREGION_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MEMORYREGION_H */
+
---
+++ MemoryRegionList.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   MemoryRegionList.h
+ * Author: Timethor
+ *
+ * Created on November 7, 2013, 6:20 AM
+ */
+
+#ifndef MEMORYREGIONLIST_H
+#define	MEMORYREGIONLIST_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MEMORYREGIONLIST_H */
+
